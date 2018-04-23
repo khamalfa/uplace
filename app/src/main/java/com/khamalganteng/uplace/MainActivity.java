2360bbfb76a2e3cfd4ed841189da7d03ff30e691
khamalfa <https://github.com/khamalfa/uplace>
2018-04-23 20:11:51 +0700

    DB dan RV Forum

--- app/src/main/java/com/khamalganteng/uplace/MainActivity.java
+++ app/src/main/java/com/khamalganteng/uplace/MainActivity.java
@@ -4,19 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import com.khamalganteng.uplace.dbHelper.OpenHelperUser;
+import com.khamalganteng.uplace.mainMenu.ForumActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    OpenHelperUser openHelperUser;
+    EditText et_mail,et_password;
+    String email,password;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
+
+        openHelperUser = new OpenHelperUser(this);
+
     }
 
     public void klikDaftar(View v) {
-        Intent intent2 = new Intent(this, SignupActivity.class);
-        startActivity(intent2);
+        intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+    public void klikLogin(View v){
+        intent = new Intent(this, ForumActivity.class);
+        startActivity(intent);
     }
 
 }
---
+++ app/src/main/java/com/khamalganteng/uplace/dbHelper/OpenHelperUser.java
@@ -0,0 +1,100 @@
+package com.khamalganteng.uplace.dbHelper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.khamalganteng.uplace.database.Posting;
+import com.khamalganteng.uplace.database.User;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OpenHelperUser extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "posting.db";
+    private static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "User";
+    public static final String COLUMN_ID = "id_user";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_UMKM_NAME = "username";
+    public static final String COLUMN_UMKM_ADDRESS = "alamat";
+    public static final String COLUMN_UMKM_DESC = "deskripsi";
+
+    public OpenHelperUser(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+
+    public OpenHelperUser(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(" CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_EMAIL + " TEXT NOT NULL, " +
+                COLUMN_PASSWORD + " TEXT NOT NULL, " +
+                COLUMN_UMKM_NAME + " TEXT NOT NULL, " +
+                COLUMN_UMKM_ADDRESS + " TEXT NOT NULL, " +
+                COLUMN_UMKM_DESC + " TEXT NOT NULL); "
+            );
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            this.onCreate(sqLiteDatabase);
+        }
+
+        public void setNewUser(User post) {
+
+            SQLiteDatabase db = this.getWritableDatabase();
+
+            //temporary value, membungkus data dengan ContentValues
+            ContentValues temp = new ContentValues();
+            temp.put(COLUMN_EMAIL, post.getEmail());
+            temp.put(COLUMN_PASSWORD, post.getPassword());
+            temp.put(COLUMN_UMKM_NAME, post.getNama_UMKM());
+            temp.put(COLUMN_UMKM_ADDRESS, post.getAlamat_UMKM());
+            temp.put(COLUMN_UMKM_DESC, post.getAlamat_UMKM());
+
+            Log.d("DB", post.getEmail());
+
+            // Masukkan
+
+            db.insert(TABLE_NAME, null, temp);
+            db.close();
+
+//        Toast.makeText(this, "Deleted successfully.", Toast.LENGTH_SHORT).show();
+
+        }
+
+        public User getUser(int id) {
+
+            String query;
+
+            query = "SELECT  * FROM " + TABLE_NAME + " ORDER BY "+ String.valueOf(id);
+
+            SQLiteDatabase db = this.getWritableDatabase();
+            Cursor cursor = db.rawQuery(query, null);
+            User post = new User();
+
+            if (cursor.moveToFirst()) {
+               do {
+                   post.setId(cursor.getInt(cursor.getColumnIndex(COLUMN_ID)));
+                   post.setEmail(cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL)));
+                   post.setPassword(cursor.getString(cursor.getColumnIndex(COLUMN_PASSWORD)));
+                   post.setNama_UMKM(cursor.getString(cursor.getColumnIndex(COLUMN_UMKM_NAME)));
+                   post.setAlamat_UMKM(cursor.getString(cursor.getColumnIndex(COLUMN_UMKM_ADDRESS)));
+                   post.setDeskripsi_UMKM(cursor.getString(cursor.getColumnIndex(COLUMN_UMKM_DESC)));
+                   } while (cursor.moveToNext());
+            }
+        return post;
+        }
+}
