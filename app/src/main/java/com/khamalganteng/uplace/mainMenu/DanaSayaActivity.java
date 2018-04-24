e92d09d7355bfd70e20ea381b53726a7f7e6b3b3
Agung Sugiarto <agungsugiarto666@gmail.com>
2018-04-24 15:11:56 +0700

    Penambahan Activity MainmEnu, tweak tampilan forum, tombol tambah di forum

--- app/src/main/java/com/khamalganteng/uplace/mainMenu/DanaSayaActivity.java
+++ app/src/main/java/com/khamalganteng/uplace/mainMenu/DanaSayaActivity.java
@@ -2,6 +2,9 @@ package com.khamalganteng.uplace.mainMenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.khamalganteng.uplace.R;
 
@@ -11,5 +14,17 @@ public class DanaSayaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dana_saya);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbardana);
+        setSupportActionBar(myToolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.danamenu, menu);
+        return true;
+    }
+
 }
