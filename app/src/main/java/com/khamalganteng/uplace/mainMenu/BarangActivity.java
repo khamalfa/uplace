6d38427274b89c6e1bd711f253e0b7c38ee70129
Agung Sugiarto <agungsugiarto666@gmail.com>
2018-04-25 16:51:40 +0700

    Activisme

--- app/src/main/java/com/khamalganteng/uplace/mainMenu/BarangActivity.java
+++ app/src/main/java/com/khamalganteng/uplace/mainMenu/BarangActivity.java
@@ -1,5 +1,6 @@
 package com.khamalganteng.uplace.mainMenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -7,14 +8,20 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.khamalganteng.uplace.R;
 import com.khamalganteng.uplace.adapter.BarangAdapter;
 import com.khamalganteng.uplace.adapter.ForumAdapter;
 import com.khamalganteng.uplace.database.Barang;
 import com.khamalganteng.uplace.dbHelper.OpenHelperBarang;
+import com.khamalganteng.uplace.mainMenu.sideMenu.PostActivity;
+import com.khamalganteng.uplace.mainMenu.sideMenu.TambahBarang;
 
 import java.util.Date;
 
@@ -61,5 +68,27 @@ public class BarangActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbarbarang);
         setSupportActionBar(myToolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.barangmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.mnu_barang:
+                Intent intento = new Intent(this, TambahBarang.class);
+                startActivity(intento);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
 
